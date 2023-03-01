903fe2570962f694f5a71e494941ed2713c1b5c4
coru89 <corey.james.keller@gmail.com>
2023-03-02 01:06:09 +0700

    js testing more

--- assets/tab-accordion.js
+++ assets/tab-accordion.js
@@ -1,7 +1,7 @@
-let accordionHeaders = document.querySelectorAll('.tab-accordion__item-header');
-const desktopContentEl = document.querySelector('.tab-accordion__content-desktop');
-
 // Get all the accordion headers
+  let accordionHeaders = document.querySelectorAll('.tab-accordion__item-header');
+  const desktopContentEl = document.querySelector('.tab-accordion__content-desktop');
+  
   if (window.innerWidth > 750) {
     if (accordionHeaders[0] && accordionHeaders[0].nextElementSibling.innerHTML && desktopContentEl) {
       desktopContentEl.innerHTML = accordionHeaders[0].nextElementSibling.innerHTML;
@@ -37,31 +37,31 @@ const desktopContentEl = document.querySelector('.tab-accordion__content-desktop
   });
   
   // Detect if the screen width changes and update the layout accordingly
-//    window.addEventListener('resize', () => {
-//     accordionHeaders = document.querySelectorAll('.tab-accordion__item-header');
+   window.addEventListener('resize', () => {
+    accordionHeaders = document.querySelectorAll('.tab-accordion__item-header');
   
-//     let containsActive = false;
-//     let activeCount = 0; // create a variable to store the count of elements with the "active" class
+    let containsActive = false;
+    let activeCount = 0; // create a variable to store the count of elements with the "active" class
     
-//     for (let i = 0; i < accordionHeaders.length; i++) {
-//       if (accordionHeaders[i].classList.contains("active")) {
-//         containsActive = true;
-//         activeCount++; // increment the active count if the current element has the "active" class
-//       }
-//     }
+    for (let i = 0; i < accordionHeaders.length; i++) {
+      if (accordionHeaders[i].classList.contains("active")) {
+        containsActive = true;
+        activeCount++; // increment the active count if the current element has the "active" class
+      }
+    }
   
-//     if (window.innerWidth >= 750) {
-//       if ((accordionHeaders[0] && desktopContentEl) && (containsActive == false || activeCount > 1)) {
-//         console.log('here');
-//         accordionHeaders.forEach(header => {
-//           header.classList.remove('active');
-//           const contentEl = header.nextElementSibling;
-//           contentEl.classList.remove('active');
-//         });
-//         desktopContentEl.innerHTML = accordionHeaders[0].nextElementSibling.innerHTML;
-//         accordionHeaders[0].classList.add('active');
-//         accordionHeaders[0].nextElementSibling.classList.add('active');
-//       } 
-//     }
-//   });
+    if (window.innerWidth >= 750) {
+      if ((accordionHeaders[0] && desktopContentEl) && (containsActive == false || activeCount > 1)) {
+        console.log('here');
+        accordionHeaders.forEach(header => {
+          header.classList.remove('active');
+          const contentEl = header.nextElementSibling;
+          contentEl.classList.remove('active');
+        });
+        desktopContentEl.innerHTML = accordionHeaders[0].nextElementSibling.innerHTML;
+        accordionHeaders[0].classList.add('active');
+        accordionHeaders[0].nextElementSibling.classList.add('active');
+      } 
+    }
+  });
   
