961cf368f846125981ea0f932afd72b1570d93c4
coru89 <corey.james.keller@gmail.com>
2023-03-03 17:33:20 +0700

    finished testimonial slider

---
+++ assets/section-testimonials.js
@@ -0,0 +1,18 @@
+var glide = new Glide('.glide', {
+  type: 'carousel',
+  autoplay: 10000,
+  gap: 30,
+  perView: 1,
+  peek: {
+    before: 180,
+    after: 180
+  },
+  breakpoints: {
+    750: {
+      peek: {
+        before: 0,
+        after: 0
+      }
+    }
+  }
+}).mount();
