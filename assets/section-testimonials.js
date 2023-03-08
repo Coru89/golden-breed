807e50bacd023af50f961ebc4e6d0585f2752756
coru89 <corey.james.keller@gmail.com>
2023-03-08 23:35:41 +0700

    glide tweaks

--- assets/section-testimonials.js
+++ assets/section-testimonials.js
@@ -1,24 +1,26 @@
-var glide = new Glide('.glide', {
-  type: 'carousel',
-  autoplay: 10000,
-  gap: 30,
-  perView: 1,
-  peek: {
-    before: 180,
-    after: 180
-  },
-  breakpoints: {
-    750: {
-      peek: {
-        before: 0,
-        after: 0
-      }
+window.addEventListener('DOMContentLoaded', (event) => {
+  var glide = new Glide('.glide', {
+    type: 'carousel',
+    autoplay: 10000,
+    gap: 30,
+    perView: 1,
+    peek: {
+      before: 180,
+      after: 180
     },
-    990: {
-      peek: {
-        before: 130,
-        after: 130
+    breakpoints: {
+      750: {
+        peek: {
+          before: 0,
+          after: 0
+        }
+      },
+      990: {
+        peek: {
+          before: 130,
+          after: 130
+        }
       }
     }
-  }
-}).mount();
+  }).mount();
+});
