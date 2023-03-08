c491968b51b728578088b89113b810c3386133e7
coru89 <corey.james.keller@gmail.com>
2023-03-08 22:39:59 +0700

    contact modals

---
+++ assets/section-shop-contact-panel.js
@@ -0,0 +1,55 @@
+
+// checking if variables are defined previously so we dont run this code again if this section is used twice
+if (
+  typeof contactModalButtons === "undefined" &&
+  typeof contactModalCloseButtons === "undefined" &&
+  typeof body === "undefined" &&
+  typeof stickyHeader === "undefined"
+  ) {
+  const contactModalButtons = document.querySelectorAll('.shop-contact__button');
+  const body = document.querySelector('body');
+  const contactModalCloseButtons = document.querySelectorAll('.shop-contact__modal-close');
+  const stickyHeader = document.querySelector('#shopify-section-header');
+    
+  contactModalButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+      const modalEl = e.currentTarget.nextElementSibling;
+
+      if (!modalEl) return;
+      const overlayEl = e.currentTarget.closest('.shop-contact__button-row').querySelector('.shop-contact__modal-overlay');
+      modalEl.classList.add('shop-contact__modal--active');
+  
+      if (!overlayEl) return;
+      overlayEl.classList.add('shop-contact__modal-overlay--active');
+
+      if (!body) return;
+      body.style.overflow = 'hidden';
+
+      if (!stickyHeader) return;
+      stickyHeader.style.display = 'none';
+  
+    });
+  });
+  
+  contactModalCloseButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+      const modalEl = e.currentTarget.closest('.shop-contact__modal');
+
+      if (!modalEl) return;
+      const overlayEl = e.currentTarget.closest('.shop-contact__button-row').querySelector('.shop-contact__modal-overlay');
+      modalEl.classList.remove('shop-contact__modal--active');
+  
+      if (!overlayEl) return;
+      overlayEl.classList.remove('shop-contact__modal-overlay--active');
+
+      if (!body) return;
+      body.style.overflow = 'auto';
+
+      if (!stickyHeader) return;
+      stickyHeader.classList.add('shopify-section-header-hidden');
+      stickyHeader.removeAttribute("style");
+  
+    });
+  });
+}
+
